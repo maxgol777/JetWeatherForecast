10ca8664e3080de5b54d2b9ff5632348aa3b0655
Maksym Golovanov <maxgol777@users.noreply.github.com>
2022-03-14 17:41:46 +0200

    Receive data

--- app/src/main/java/com/example/jetweatherforecast/repository/WeatherRepository.kt
+++ app/src/main/java/com/example/jetweatherforecast/repository/WeatherRepository.kt
@@ -1,16 +1,20 @@
 package com.example.jetweatherforecast.repository
 
+import android.util.Log
 import com.example.jetweatherforecast.data.DataOrException
+import com.example.jetweatherforecast.model.Weather
 import com.example.jetweatherforecast.model.WeatherObject
 import com.example.jetweatherforecast.network.WeatherApi
 import javax.inject.Inject
 
 class WeatherRepository @Inject constructor(private val api: WeatherApi) {
 
-    suspend fun getWeather(cityQuery: String): DataOrException<WeatherObject, Exception> = try {
+    suspend fun getWeather(cityQuery: String): DataOrException<Weather, Exception> = try {
         val response = api.getWeather(query = cityQuery)
+        Log.d("PEDRO", "getWeather: ${response.city.country}")
         DataOrException(data = response)
     } catch (exception: Exception) {
+        Log.d("PEDRO", "getWeather: ${exception}")
         DataOrException(e = exception)
     }
 
--- app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
@@ -1,10 +1,34 @@
 package com.example.jetweatherforecast.screens.main
 
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.jetweatherforecast.data.DataOrException
+import com.example.jetweatherforecast.model.Weather
 
 @Composable
-fun MainScreen(navController: NavController) {
-    Text("Pedrilo hahaha !!!")
+fun MainScreen(
+    navController: NavController,
+    mainViewModel: MainViewModel = hiltViewModel()
+) {
+    ShowData(mainViewModel)
+}
+
+
+@Composable
+fun ShowData(mainViewModel: MainViewModel) {
+    val weatherData = produceState<DataOrException<Weather, Exception>>(
+        initialValue = DataOrException(loading = true)
+    ) {
+        value = mainViewModel.getWeatherData(city = "Seattle")
+    }.value
+
+    if (weatherData.loading == true) {
+        CircularProgressIndicator()
+    } else if (weatherData.data != null) {
+        Text(text = "Main Screen ${weatherData.data}")
+    }
 }
