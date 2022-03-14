32610b088a8d1567b15b065b339b8dfd071fa513
Maksym Golovanov <maxgol777@users.noreply.github.com>
2022-03-14 20:49:23 +0200

    App Bar

--- app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
@@ -1,25 +1,22 @@
 package com.example.jetweatherforecast.screens.main
 
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.jetweatherforecast.WeatherApp
 import com.example.jetweatherforecast.data.DataOrException
 import com.example.jetweatherforecast.model.Weather
+import com.example.jetweatherforecast.widgets.WeatherAppBar
 
 @Composable
 fun MainScreen(
     navController: NavController,
     mainViewModel: MainViewModel = hiltViewModel()
 ) {
-    ShowData(mainViewModel)
-}
-
-
-@Composable
-fun ShowData(mainViewModel: MainViewModel) {
     val weatherData = produceState<DataOrException<Weather, Exception>>(
         initialValue = DataOrException(loading = true)
     ) {
@@ -29,6 +26,20 @@ fun ShowData(mainViewModel: MainViewModel) {
     if (weatherData.loading == true) {
         CircularProgressIndicator()
     } else if (weatherData.data != null) {
-        Text(text = "Main Screen ${weatherData.data}")
+        MainScaffold(weather = weatherData.data!!, navController)
     }
+}
+
+@Composable
+fun MainScaffold(weather: Weather, navController: NavController) {
+    Scaffold(topBar = {
+        WeatherAppBar(title = "Pedrilo HAHAHA")
+    }) {
+        MainContent(data = weather)
+    }
+}
+
+@Composable
+fun MainContent(data: Weather) {
+    Text(text = data.city.name)
 }
